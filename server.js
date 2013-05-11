e702435f9bb33059dc092cf00acff3af08b1ff94
Kevin Hwang <k3vinhwang@gmail.com>
2013-05-10 23:47:25 -0700

    Use EJS because it's THE FUTURE and also configure express

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 
 io.set("log level", 1);
 
+// Express configuration
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.bodyParser());
+app.use(express['static'](__dirname + "/static")); //static is a keyword so it's referenced like this for my linter (:
+
 app.get("/", function (req, res) {
-  res.sendfile(__dirname + "/index.html");
+  res.render("index");
 });
 
 io.sockets.on("connection", function (socket) {
