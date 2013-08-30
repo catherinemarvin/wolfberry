6b7c71d27636da2b8d10e52ff948039f8aebf25d
Kevin Hwang <k3vinhwang@gmail.com>
2013-08-29 21:40:02 -0700

    Add easy way to join a room from the index page.

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
   $("#openRoomBtn").click(function () {
     $.post("createRoom", function (data) {
       console.log(data);
-      window.location.href = "/room/"+data.roomId;
+      window.location.href = "/room/" + data.roomId;
     });
+  });
+
+  $("#joinRoom").click(function () {
+    var roomNum = $("#roomId").val();
+    window.location.href = "/room/"+ roomNum;
+
   });
 });
